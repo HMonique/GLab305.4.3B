2974250a775be167fdd0dce72685e2bee80c68cf
HMonique <moniquehl@outlook.com>
2024-09-17 15:13:51 -0400

    second commit

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -6,6 +6,23 @@ import java.util.Date;
 
 @Entity
 @Table
+//Using @NamedQuery for single  HQL
+@NamedQuery(name = "getAllEmployees", query = "select e from Employee e")
+//Using @NamedQueries for multiple  HQL
+@NamedQueries({
+        @NamedQuery(name = "getEmployeeById", query = "select e.name,e.salary,e.job from Employee e where id=:id"),
+        @NamedQuery(name = "getAllDepartments", query = "from Employee"),
+        @NamedQuery(name = "findEmployeeByName", query = "from Employee e where e.name = :name"),
+        @NamedQuery(name = "employeeDeptAlias", query = "select e, e.officeCode , e.name, " +
+                "CASE " +
+                "When (e.officeCode = 1) THEN 'IT'"
+                + "WHEN (e.officeCode = 6) THEN 'Admin'"
+                + "WHEN (e.officeCode = 5) THEN 'HR'"
+                + "WHEN (e.officeCode = 4) THEN 'Developers'"
+                + "WHEN(e.officeCode = 3) THEN 'Accounts'"
+                + "WHEN (e.officeCode =2) THEN 'Finance'"
+                + "ELSE 'General' END FROM Employee e  "),
+})
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,57 +35,75 @@ public class Employee {
     private String city;
     private Date startDate;
     private int officeCode;
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getSalary() {
         return salary;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
     public String getJob() {
         return job;
     }
+
     public void setJob(String job) {
         this.job = job;
     }
+
     public String getAddressLine() {
         return addressLine;
     }
+
     public void setAddressLine(String addressLine) {
         this.addressLine = addressLine;
     }
+
     public String getZipcode() {
         return zipcode;
     }
+
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public Date getStartDate() {
         return startDate;
     }
+
     public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
-    public int getOfficeCode(){
+
+    public int getOfficeCode() {
         return officeCode;
     }
+
     public void setOfficeCode(int officeCode) {
         this.officeCode = officeCode;
     }
@@ -79,3 +114,4 @@ public class Employee {
                 + addressLine + ", zipcode=" + zipcode + ", city=" + city + ", startDate=" + startDate + "]";
     }
 }
+
